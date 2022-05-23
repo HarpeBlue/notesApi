46c3f4872eafe7201c961ec887aec33fb46ac406
HarpeBlue <hspe2507@gmail.com>
2022-05-23 08:55:09 -0500

    :heavy_plus_sign: Setup express some endpoints

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-const http = require("http");
+const express = require("express");
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World\n");
+let notes = [
+  {
+    id: 1,
+    title: "First note",
+    body: "This is my first note",
+  },
+  {
+    id: 2,
+    title: "Second note",
+    body: "This is my second note",
+  },
+  {
+    id: 3,
+    title: "Third note",
+    body: "This is my third note",
+  },
+];
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  note ? res.json(note) : res.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  const ids = notes.map((note) => note.id);
+  const maxId = Math.max(...ids);
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date().toISOString(),
+  };
+  notes = [...notes, newNote];
+  res.status(201).json(newNote);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
